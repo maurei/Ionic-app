7e18c1288ee3300788190cb0db0fa68325796aac
Maurits Moeys <maurits.moeys@gmail.com>
2015-03-09 16:04:30 -0700

    added mobile file in slideboxapp

---
+++ slideboxapp/www/js/available.js
@@ -0,0 +1,84 @@
+racts.factory('availableModel', [function() {
+
+	var data = {
+		list: []
+	}
+
+	return data;
+
+}]);
+
+
+
+racts.service('availableResolver', ['$http', '$q', 'availableModel','session', function($http, $q, availableModel, session ) {
+	  console.log(session.currentUser().id)
+
+		var getAvailable = $http.get('http://localhost:3000/categories/?user_id=' + session.currentUser().id)
+
+				.success(function(response) {
+					availableModel.list = response.list
+				})
+				.error(function(response){
+					console.log('error')
+				})
+
+	return getAvailable
+
+}])
+
+
+racts.service('availableService', ['$http', '$q', 'availableModel', 'session', function($http, $q, availableModel, session ) {
+
+
+
+	this.subscribe = function(category){
+	  $http.post('/users/' + session.currentUser().id + '/categories/' + category.id + '/subscribe')
+	  	.success(function(response) {
+	  		console.log(response)
+	  	})
+	  	.error(function(response) {
+	  		console.log("ERROR!")
+	  	})
+	}
+	this.availableModel = availableModel
+
+
+}])
+
+
+
+racts.controller('availableController', ['$http', '$scope', 'availableService', 'availableResolver', 'loadAvailableTasksService', function($http, $scope, availableService, availableResolver, loadAvailableTasksService){
+
+	$scope.available = availableService.availableModel.list
+
+
+	console.log('showing available... or not?')
+  $scope.showTasks = function(index){
+		var category = availableService.availableModel.list[index]
+		loadAvailableTasksService.load(category.id)
+  }
+
+  $scope.subscribe = function(category, index) {
+  	availableService.subscribe(category)
+  }
+}])
+
+
+
+racts.service('loadAvailableTasksService', ['$state','$http', '$q', 'availableModel', function($state, $http, $q, availableModel ){
+
+
+		this.load = function(id){
+			$http.get('http://localhost:3000/categories/'+id)
+				.success(function(response) {
+					var paramsJSON = JSON.stringify(response)
+					$state.go('landingpage.showtasks', {tasks: paramsJSON})
+
+				})
+				.error(function(response){
+					console.log('error')
+				})
+		}
+
+}])
+
---
+++ slideboxapp/www/js/showtasks.js
@@ -0,0 +1,9 @@
+
+
+racts.controller('showTasksController', ['$stateParams','$http', '$scope',function($stateParams, $http, $scope){
+
+	console.log('showtask launched!')
+  var originalParams = JSON.parse($stateParams.tasks)
+	$scope.tasks = originalParams
+
+}])
