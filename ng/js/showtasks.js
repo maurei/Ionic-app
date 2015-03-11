62bfe921a2a7575473ebf8c306d780543d493909
Maurits Moeys <maurits.moeys@gmail.com>
2015-03-11 12:00:15 -0700

    mobile app

--- ng/js/showtasks.js
+++
@@ -1,8 +0,0 @@
-
-
-racts.controller('showTasksController', ['$stateParams','$http', '$scope',function($stateParams, $http, $scope){
-
-  var originalParams = JSON.parse($stateParams.tasks)
-	$scope.tasks = originalParams
-
-}])
--- slideboxapp/www/js/showtasks.js
+++
@@ -1,9 +0,0 @@
-
-
-racts.controller('showTasksController', ['$stateParams','$http', '$scope',function($stateParams, $http, $scope){
-
-	console.log('showtask launched!')
-  var originalParams = JSON.parse($stateParams.tasks)
-	$scope.tasks = originalParams
-
-}])
